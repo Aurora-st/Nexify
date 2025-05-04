33089f3687a5ffcbc761b5ae7ad3b00384db6d78
Aurora-st <abhinavsingh.official05@gmail.com>
2025-05-04 17:01:40 +0530

    Add files via upload

---
+++ coming/app/src/main/java/com/example/coming/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.coming; // Change this to your actual package name
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
